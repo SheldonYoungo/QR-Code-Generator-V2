2b737afaa4995547e67a72d5a72e881235fb5321
SheldonYoungo <sheldomyoungo@gmail.com>
2023-11-22 15:19:06 -0400

    Code commented

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,12 +5,16 @@ const qrImage = require('../controllers/qrImage')
 const router = Router()
 
 router.get('/', (req, res) => {
+  // Send the HTML file to the client
   return res.sendFile(path.resolve(__dirname, '../dist', 'index.html'))
 })
 
+// Downloads the qr-code as an .png file
 router.get('/download/:imageName', qrImage.download)
+// Sends the qr-code to the client to be shown
 router.get('/qr-codes/:qrCode', qrImage.show)
 
+// Generates a qr-code and returns it as an .png file
 router.post('/createQrImage', qrImage.create)
 
 module.exports = router
--- server/services/scripts.js
+++ server/services/scripts.js
@@ -12,29 +12,40 @@ const urlExtentions = [
   '.co'
 ] // Parameter to check if the QR name match one of this common domain extentions
 
-function createQrImage (url) {
+function createQrImage(url) {
+  // Generate QR code iamge form url
   const qrCode = qr.image(url)
+
+  // Get domain from url
   let domain = new URL(url).hostname
+
+  // Trim domain to get only its name
   domain = domain.substring(domain.lastIndexOf('.', domain.lastIndexOf('.') - 1) + 1) //  Trimming the domain to get only its name
 
+  // Set QR image file name
   const qrImageFileName = `${domain}_qr_code.png`
+
+  // Set QR image location
   const qrImageLocation = `./public/qr-codes/${qrImageFileName}`
 
+  // Delete old QR codes with similar names
   deleteFilesWithNamesContaining('./public/qr-codes/', urlExtentions)
+
+  // Write QR code image to file location
   qrCode.pipe(fs.createWriteStream(qrImageLocation))
 
   return qrImageFileName
 }
 
-function deleteFilesWithNamesContaining (directoryPath, nameParts) {
-  // Lee el contenido del directorio
+function deleteFilesWithNamesContaining (directoryPath, extensions = []) {
+  // Reads directory content
   fs.readdir(directoryPath, (err, files) => {
     if (err) throw err
 
-    // Itera sobre los archivos en el directorio
+    // Iterate over files in the directory
     for (const file of files) {
-      // Verifica si el nombre del archivo contiene una de las cadenas en el array nameParts
-      if (nameParts.some(namePart => file.includes(namePart))) {
+      // Verifies if the file name extension matches one of the extensions
+      if (extensions.some(namePart => file.includes(namePart))) {
         // Elimina el archivo
         fs.unlink(path.join(directoryPath, file), err => {
           if (err) throw err
