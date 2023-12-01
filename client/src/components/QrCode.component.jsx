9feaaab66baa35de98bdf537eac5c8e52b391fd9
SheldonYoungo <sheldomyoungo@gmail.com>
2023-12-01 12:34:28 -0400

    Add image loading state to QrCode component

--- client/src/components/QrCode.component.jsx
+++ client/src/components/QrCode.component.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import saveAs from 'file-saver'
 
 import { SERVER_URL } from "../utils/const"
 
 export function QrCode({ imgName }) {
+  const [isLoading, setIsLoading] = useState(true)
+
  const handleDownload = () => {
   event.preventDefault()
   fetch(`${SERVER_URL}/download/${imgName}`)
@@ -11,17 +13,23 @@ export function QrCode({ imgName }) {
     .then(blob => saveAs(blob, imgName))
  }
 
+ const handleImageLoad = () => {
+  setIsLoading(false);
+}
+
  return (
     <>
       <div>
+
+        {isLoading && <h3>Creating image...</h3>}
         <img
           src={`${SERVER_URL}/qr-codes/${imgName}`}
           alt="QR Code"
+          onLoad={handleImageLoad}
         />
         <br></br>
         <button className='download-btn' onClick={handleDownload}>Descargar</button>
       </div>
-
     </>
  )
 }
